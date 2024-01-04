33416d5bcc43258e860645aee9e22ec37a0b66c4
Lynn <cpelite@zlatina.tech>
2024-01-04 16:49:58 +0100

    Add files via upload

---
+++ audiorestart.ps1
@@ -0,0 +1,9 @@
+﻿
+$con = Read-Host "Do you want to restart Audio services? [Y, N]"
+if ($con -eq 'Y') {
+    Restart-Service -Force -Name AudioEndpointBuilder
+    Restart-Service -Force -Name Audiosrv
+    Write-Output "Audio services have been restarted."
+} else {
+    Write-Output "Exiting!"
+}
